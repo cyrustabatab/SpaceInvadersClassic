8226b456d22cc34a870de76baae26abf1420e435
Cyrus Tabatabai-Yazdi <24766642+cyrustabatab@users.noreply.github.com>
2021-04-04 21:43:12 -0700

    use different hues for flying saucer by reading from spritesheet

--- enemy_spaceship.py
+++ enemy_spaceship.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import random
 import time
+from spritesheet import Spritesheet
 
 vec = pygame.math.Vector2
 
@@ -10,11 +11,13 @@ GREEN = (0,255,0)
 def get_images(directory,size=60):
     images= []
     for file_ in os.listdir(directory):
-        image = pygame.image.load(os.path.join(directory,file_)).convert_alpha()
-        if size != -1:
-            image = pygame.transform.scale(image,(size,size))
+        path = os.path.join(directory,file_)
+        if os.path.isfile(path):
+            image = pygame.image.load(path).convert_alpha()
+            if size != -1:
+                image = pygame.transform.scale(image,(size,size))
 
-        images.append(image)
+            images.append(image)
 
 
     return images
@@ -198,6 +201,35 @@ class RocketShip(EnemySpaceShip):
             self.image = self.images[self.image_index]
             self.frame_count = 0
 
+def get_flying_saucer_images():
+    
+    '''load from spritesheet the different hues'''
+    example_images = get_images(os.path.join('assets','flying_saucer'),size=-1)
+
+    spritesheets_directory = os.path.join("assets",'flying_saucer','spritesheets')
+    
+    num_rows = 6
+    different_images = [example_images]
+    width,height = 40,30
+    for spritesheet in os.listdir(spritesheets_directory):
+        path = os.path.join(spritesheets_directory,spritesheet)
+        sheet = Spritesheet(path)
+        images = [] 
+        for row in range(num_rows):
+            image = sheet.load_strip((0,row * height,width,height),1)[0] # returns a list but we know its 1 image
+            images.append(image)
+        different_images.append(images)
+    return different_images
+
+
+
+
+
+
+
+
+
+
 
 
 class FlyingSaucer(RocketShip):
@@ -205,9 +237,10 @@ class FlyingSaucer(RocketShip):
     '''behavior of flying saucer is very similar to rocket ship except update method will be slightly differen in future'''
 
 
-    images = get_images(os.path.join('assets','flying_saucer'))
+    hue_images = get_flying_saucer_images()
 
     def __init__(self,screen_width,screen_height,speed=3,health=100):
+        self.images = random.choice(self.hue_images)
         super().__init__(screen_width,screen_height,speed,health)
 
         self.x_speed = random.choice((-1,1)) * random.randint(3,5)
---
+++ spritesheet.py
@@ -0,0 +1,40 @@
+import pygame
+
+
+
+
+class Spritesheet:
+
+
+    def __init__(self,filename):
+        try:
+            self.sheet = pygame.image.load(filename).convert_alpha()
+        except:
+            print("Unable to read spritesheet")
+
+
+    def image_at(self,rectangle,colorkey=None):
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size,pygame.SRCALPHA)
+        image.blit(self.sheet,(0,0),rect)
+        if colorkey is not None:
+            if colorkey == -1:
+                colorkey = image.get_at((0,0))
+            image.set_colorkey(colorkey,pygame.RLEACCEL)
+        return image
+
+
+    def images_at(self,rects,colorkey=None):
+
+        return [self.image_at(rect,colorkey) for rect in rects]
+
+
+    def load_strip(self,rect,image_count,colorkey=None):
+
+        rects = [(rect[0] + rect[2] * x,rect[1],rect[2],rect[3]) for x in range(image_count)]
+
+        return self.images_at(rects,colorkey)
+
+
+
+
